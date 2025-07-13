45b81095091d137091a56e76b1dfcecfe8fefeab
nabilarahhhhma <nabilarahmadani090@gmail.com>
2025-07-13 10:04:34 +0700

    Update login.py

--- login.py
+++ login.py
@@ -10,23 +10,15 @@ user_db = {
 
 def login_form():
     st.markdown("""
-        <h2 style='text-align:center;'>🎀 Yuk Masuk ke Cute App 🎀</h2>
-        <p style='text-align:center;'>Silakan masuk ke <b>Cute Calculus App</b> untuk memulai perhitunganmu!</p>
-    """, unsafe_allow_html=True)
-
-    st.markdown("""
     <div style='text-align: center;'>
         <img src='https://cdn-icons-png.flaticon.com/512/847/847969.png' width='80'>
     </div>
+    <h2 style='text-align:center;'>🎀 Hallo, Selamat Datang! 🎀</h2>
+    <p style='text-align:center;'>Silakan masuk ke <b>Cute Calculus App</b> untuk memulai perhitunganmu!</p>
     """, unsafe_allow_html=True)
 
     username = st.text_input("👑 Username")
     password = st.text_input("🔒 Password", type="password")
-
-    st.markdown("### 🖼️ Upload siluetmu (opsional)")
-    uploaded_avatar = st.file_uploader("Upload gambar (PNG/JPG)", type=["png", "jpg", "jpeg"])
-    if uploaded_avatar:
-        st.image(uploaded_avatar, width=100, caption="Siluet kamu 💖")
 
     if st.button("✨ Login"):
         if username in user_db and user_db[username] == hash_password(password):
